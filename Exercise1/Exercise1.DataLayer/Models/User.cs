86b2a11e899005748a7869f8076f80b85a16ea82
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-28 19:39:34 +0100

    Create user model

---
+++ Exercise1/Exercise1.DataLayer/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise1.DataLayer.Models
+{
+    public class User
+    {
+        public User(int? id, string email, string firstName, string? lastName, int? age, string? avatarUrl)
+        {
+            Id = id;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            AvatarUrl = avatarUrl;
+        }
+
+        public int? Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public string? AvatarUrl { get; set; }
+    }
+}
