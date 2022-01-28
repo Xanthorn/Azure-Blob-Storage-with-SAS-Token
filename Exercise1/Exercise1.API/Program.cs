fb84d5d97f353e074b219dfebae200e828cfffc4
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-28 21:07:44 +0100

    Add UserRepository as a singleton

--- Exercise1/Exercise1.API/Program.cs
+++ Exercise1/Exercise1.API/Program.cs
@@ -1,3 +1,6 @@
+using Exercise1.DataLayer.Repositories;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+IUserRepository userRepository = new UserRepository(ConfigurationManager.ConnectionStrings["AzureSqlServer"].ConnectionString);
+builder.Services.AddSingleton(userRepository);
 
 var app = builder.Build();
 
