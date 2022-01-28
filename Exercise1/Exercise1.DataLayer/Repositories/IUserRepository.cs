6c339aa9a93155d9ab756dfef5f8ae787ac33fa7
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-28 19:42:04 +0100

    Create interface for user repository

---
+++ Exercise1/Exercise1.DataLayer/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Exercise1.DataLayer.Models;
+
+namespace Exercise1.DataLayer.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetUser(int id);
+        List<User> GetUsers();
+        int AddUser(User user);
+        int UpdateUser(User user);
+    }
+}
