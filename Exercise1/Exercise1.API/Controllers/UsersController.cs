f72b0921f24025c142dda14c1a04b516db58b6b6
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-29 14:26:24 +0100

    Add IBlobContainerService to UsersController

--- Exercise1/Exercise1.API/Controllers/UsersController.cs
+++ Exercise1/Exercise1.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Exercise1.API.Contracts;
 using Exercise1.API.Contracts.User.Add;
 using Exercise1.API.Contracts.User.Edit;
+using Exercise1.DataLayer.Azure.StorageAccount;
 using Exercise1.DataLayer.Models;
 using Exercise1.DataLayer.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -11,43 +12,69 @@ namespace Exercise1.API.Controllers
     public class UsersController
     {
         private readonly IUserRepository _userRepository;
+        private readonly IBlobContainerService _blobContainerService;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IBlobContainerService blobContainerService)
         {
             _userRepository = userRepository;
+            _blobContainerService = blobContainerService;
         }
 
         [HttpGet(ApiRoutes.User.GetAll)]
         public List<User> GetUsers() => _userRepository.GetUsers();
 
         [HttpGet(ApiRoutes.User.GetById)]
-        public User GetUserById([FromRoute] int id) => _userRepository.GetUser(id);
+        public User GetUserById([FromRoute] int id)
+        {
+            User user = _userRepository.GetUser(id);
+
+            if (user.AvatarName != null)
+            {
+                user.AvatarName = _blobContainerService.GetImageUri(user.AvatarName);
+            }
+
+            return user;
+        }
 
         [HttpPost(ApiRoutes.User.Add)]
-        public int AddUser([FromBody] AddUserRequest request)
+        public async Task<int> AddUser([FromForm] AddUserRequest request)
         {
+            string avatarName = null;
+            if (request.Avatar != null)
+            {
+                avatarName = await _blobContainerService.UploadImage(request.Avatar);
+            }
+
             User user = new()
             {
                 Email = request.Email,
                 FirstName = request.FirstName,
                 LastName = request.LastName,
                 Age = request.Age,
-                AvatarUrl = request.AvatarUrl
+                AvatarName = avatarName
             };
             return _userRepository.AddUser(user);
         }
 
         [HttpPut(ApiRoutes.User.Update)]
-        public int UpdateUser([FromRoute] int id, [FromBody] EditUserRequest request)
+        public async Task<int> UpdateUser([FromRoute] int id, [FromForm] EditUserRequest request)
         {
-            User user = new()
+            User user = _userRepository.GetUser(id);
+
+            string newAvatarName = null;
+            if (request.Avatar != null)
+            {
+                newAvatarName = await _blobContainerService.UpdateImage(request.Avatar, user.AvatarName);
+            }
+
+            user = new()
             {
                 Id = id,
                 Email = request.Email,
                 FirstName = request.FirstName,
                 LastName = request.LastName,
                 Age = request.Age,
-                AvatarUrl = request.AvatarUrl
+                AvatarName = newAvatarName
             };
             return _userRepository.UpdateUser(user);
         }
