e77f35aacbe1e4596504234d33a5d38a83553069
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-29 14:25:11 +0100

    Add implementation of IBlobContainerService to service collection

--- Exercise1/Exercise1.API/Program.cs
+++ Exercise1/Exercise1.API/Program.cs
@@ -1,3 +1,4 @@
+using Exercise1.DataLayer.Azure.StorageAccount;
 using Exercise1.DataLayer.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,11 @@ builder.Services.AddSwaggerGen();
 
 IUserRepository userRepository = new UserRepository(configuration.GetValue<string>("ConnectionStrings:AzureSqlServer"));
 builder.Services.AddSingleton(userRepository);
+
+BlobContainerSettings blobContainerSettings = new();
+configuration.Bind(nameof(BlobContainerSettings), blobContainerSettings);
+IBlobContainerService blobContainerService = new BlobContainerService(blobContainerSettings);
+builder.Services.AddSingleton(blobContainerService);
 
 var app = builder.Build();
 
