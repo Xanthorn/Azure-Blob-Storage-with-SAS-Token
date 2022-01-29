f5eb979adccd563b6c0ffe0261e29a73a09d9752
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-29 13:42:38 +0100

    Implement IBlobContainerService

--- Exercise1/Exercise1.DataLayer/Azure/StorageAccount/IBlobContainerService.cs
+++ Exercise1/Exercise1.DataLayer/Azure/StorageAccount/IBlobContainerService.cs
@@ -5,7 +5,7 @@ namespace Exercise1.DataLayer.Azure.StorageAccount
     public interface IBlobContainerService
     {
         public Task<string> UploadImage(IFormFile image);
-        public Task<string> UpdateImage(IFormFile image, string oldImageUri);
+        public Task<string> UpdateImage(IFormFile image, string oldImageName);
         public string GetImageUri(string imageName);
     }
 }
