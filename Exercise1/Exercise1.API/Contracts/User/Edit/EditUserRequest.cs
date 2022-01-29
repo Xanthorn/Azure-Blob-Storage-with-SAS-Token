7fa480001e906964d4977665d9160caef015c7d2
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-29 14:36:32 +0100

    Add nullable properties to add/edit user

--- Exercise1/Exercise1.API/Contracts/User/Edit/EditUserRequest.cs
+++ Exercise1/Exercise1.API/Contracts/User/Edit/EditUserRequest.cs
@@ -4,8 +4,8 @@
     {
         public string Email { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-        public IFormFile Avatar { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public IFormFile? Avatar { get; set; }
     }
 }
